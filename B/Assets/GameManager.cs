dc44aeebb87bd00c123efae1a96ed419aa7969b7
jmjeon8258 <jmjeon9923@gmail.com>
2022-08-09 22:12:54 +0900

    모시깽하고싶음

--- B/Assets/GameManager.cs
+++ B/Assets/GameManager.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     Silder slider;
-    public int supplyMoneySpeed = 5;
+    friendly friend;
+    Enemy Enem;
+    public int supplyMoneySpeed = 2;
     public int supplyMoneyDecrease = 55;
+
+
+    public static int count;
+
+    public GameObject friendly;
+    public GameObject Enemy;
 
     #region ¼Ò¸ð°ª
     public int firstbuttonDecrease = 10;
     public int secondbuttonDecrease = 25;
     public int thirdbuttonDecrease = 50;
+    public int fourthbuttonDecrease = 75;
+    public int fifthbuttonDecrease = 100;
+
     #endregion
 
     void Start()
     {
         slider = GameObject.Find("MoneySlider").GetComponent<Silder>();
+        friend = GetComponent<friendly>();
+        Enem = GetComponent<Enemy>();
         StartCoroutine(Money());
+        StartCoroutine(Count());
     }
 
     
@@ -29,7 +43,7 @@ public class GameManager : MonoBehaviour
     IEnumerator Money()
     {
         slider.slMoney.value += supplyMoneySpeed;
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.5f);
         StartCoroutine(Money());
     }
     public void supplySpeed()
@@ -37,8 +51,8 @@ public class GameManager : MonoBehaviour
         if (slider.slMoney.value >= supplyMoneyDecrease)
         {
             slider.slMoney.value -= supplyMoneyDecrease;
-            supplyMoneyDecrease += 20;
-            supplyMoneySpeed += 7;
+            supplyMoneyDecrease += 30;
+            supplyMoneySpeed += 3;
         }
     }
     public void firstbutton()
@@ -62,5 +76,28 @@ public class GameManager : MonoBehaviour
         {
             slider.slMoney.value -= thirdbuttonDecrease;
         }
+    }
+    public void fourthbutton()
+    {
+        if (slider.slMoney.value >= fourthbuttonDecrease)
+        {
+            slider.slMoney.value -= fourthbuttonDecrease;
+        }
+    }
+    public void fifthbutton()
+    {
+        if(slider.slMoney.value >= fifthbuttonDecrease)
+        {
+            slider.slMoney.value -= fifthbuttonDecrease;
+        }
+    }
+    IEnumerator Count()
+    {
+        yield return new WaitForSecondsRealtime(1);
+        count++;
+    }
+    public void SummonButton()
+    {
+
     }
 }
--- B/Assets/friendly.cs
+++ B/Assets/friendly.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class friendly : MonoBehaviour
 {
     [SerializeField] Transform[] linePos;
     public float speed = 2f;
     int lineNum = 0;
-    public int AttackDamage;
+    public static int AttackDamage;
+    public Text Damage;
+    Enemy enemy;
     void Start()
     {
+        enemy = GameObject.Find("Enemy").GetComponent<Enemy>();
         transform.position = linePos[lineNum].transform.position;
     }
     void Update()
     {
-        MovePath();
+        if (CompareTag("Friend"))
+        {
+            MovePath();
+            HPmove();
+        }
     }
     public void MovePath()
     {
@@ -31,6 +39,11 @@ public class friendly : MonoBehaviour
         }
 
     }
+    void HPmove()
+    {
+        Damage.text = $"{AttackDamage}";
+        Damage.transform.position = Camera.main.WorldToScreenPoint(transform.position);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy")) //부딛힌 것이 적이면서
@@ -39,6 +52,7 @@ public class friendly : MonoBehaviour
                 //공격력이 적 체력보다 크다면
             {
                 Destroy(collision.gameObject);
+                Destroy(enemy.HP);
                 //적 파괴
             }
             else if(collision.gameObject.GetComponent<Enemy>().EnemyHp >= AttackDamage)
@@ -48,6 +62,7 @@ public class friendly : MonoBehaviour
                     collision.gameObject.GetComponent<Enemy>().EnemyHp - AttackDamage;
                 //적 체력을 공격력만큼 깎고
                 Destroy(gameObject);
+                Destroy(Damage);
                 //자신 파괴
             }
         }
