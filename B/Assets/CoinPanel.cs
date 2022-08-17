e3564437cb971b2a53113fda6414fa751be262ef
jmjeon5533 <jmjeon5533@gmail.com>
2022-08-17 19:45:19 +0900

    .

--- B/Assets/CoinPanel.cs
+++ B/Assets/CoinPanel.cs
@@ -10,12 +10,17 @@ public class CoinPanel : MonoBehaviour, IPointerDownHandler
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        manager.GetCoin();
+/*
         if (manager.targetIndex == 0)
         {
-            manager.GetCoin();
+            
+            
             Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             pos += new Vector2(0, 0.3f);
             Instantiate(TouchEffect, pos, Quaternion.identity);
+            
         }
+*/
     }
 }
